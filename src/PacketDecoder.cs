6d808668fe09516ffdfd12e33d86abe62e528f91
Dan Nilsson <raxptor@gmail.com>
2016-11-09 16:39:46 +0100

    - mbore

--- src/PacketDecoder.cs
+++ src/PacketDecoder.cs
@@ -1,7 +1,12 @@
 namespace Cube
 {
+	public interface PacketDecoder
+	{
+		int Decode(byte[] data, int offset, int length, out Netki.DecodedPacket pkt);
+	}
+
 	public interface ApplicationPacketHandler : PacketDecoder
-	{		
+	{
 		 Netki.Bitstream.Buffer MakePacket(Netki.Packet packet);
 	}
 }
