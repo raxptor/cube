e931e0155f6908a457c29c64f4d8eff286b13bbb
Dan Nilsson <raxptor@gmail.com>
2017-01-15 15:16:46 +0100

    - change port

--- src/NodeMaster.cs
+++ src/NodeMaster.cs
@@ -7,8 +7,8 @@ namespace Cube
 {
 	public class NodeMaster
 	{
-		public const int DEFAULT_NODE_PORT = 8100;
-		public const int DEFAULT_CLIENT_PORT = 8101;
+		public const int DEFAULT_NODE_PORT = 8400;
+		public const int DEFAULT_CLIENT_PORT = 8401;
 
 		class NodeRecord
 		{
